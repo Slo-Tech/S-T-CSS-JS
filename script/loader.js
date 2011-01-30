b17f7a47203085c6fc31cea6fa4329cffdff498a
Primoz <primoz@slo-tech.com>
2011-01-30 18:24:09 +0100

    nov lightbox loader

--- script/loader.js
+++ script/loader.js
@@ -1,8 +1,8 @@
-document.write("<script src='http://st.fey/script/lightbox_plus.js'><\/script>");
 document.write("<script src='http://st.fey/script/js_quicktags.js'><\/script>");
 document.write("<script src='http://st.fey/script/legacy.js'><\/script>");
 
 document.write("<script src='http://st.fey/script/jquery.js'><\/script>");
+document.write("<script src='http://st.fey/script/jquery.prettyPhoto.js'><\/script>");
 document.write("<script src='http://st.fey/script/jquery.editable.js'><\/script>");
 document.write("<script src='http://st.fey/script/jquery.timers.js'><\/script>");
 document.write("<script src='http://st.fey/script/jquery.hoverIntent.js'><\/script>");
