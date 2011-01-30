17553aa59bdadb4a03b357c0b6963f85e8e78bd5
Jure Cuhalev <gandalfar@gmail.com>
2011-01-30 18:58:25 +0100

    fancy upload files
    Signed-off-by: Jure Cuhalev <gandalfar@gmail.com>

--- script/loader.js
+++ script/loader.js
@@ -10,4 +10,11 @@ document.write("<script src='http://st.fey/script/jquery.form.js'><\/script>");
 document.write("<script src='http://st.fey/script/jquery.cookie.js'><\/script>");
 document.write("<script src='http://st.fey/script/jquery.textarearesizer.js'><\/script>");
 
+/* fancy upload */
+document.write("<link rel='stylesheet' href='http://st.fey/css/jquery-ui.css' id='theme'>");
+document.write("<link rel='stylesheet' href='http://st.fey/css/jquery.fileupload-ui.css'>");
+document.write("<script src='http://st.fey/script/jquery-ui.js'><\/script>");
+document.write("<script src='http://st.fey/script/jquery.fileupload.js'><\/script>");
+document.write("<script src='http://st.fey/script/jquery.fileupload-ui.js'><\/script>");
+
 document.write("<script src='http://st.fey/script/scripts.js'><\/script>");
