ef2054bc7fd48ec74863db8790332a368fadbbbe
Jure Cuhalev <gandalfar@gmail.com>
2011-02-05 22:36:52 +0100

    removed unused code from legacy.js

--- script/legacy.js
+++ script/legacy.js
@@ -1,183 +1,100 @@
-	function onLoad(func)
-	{
-		if(window.addEventListener)
-		{
-			window.addEventListener('load', func, false);
-		}
-		else if (window.attachEvent)
-		{
-			window.attachEvent('onload', func);
-		}
-	}
-
-
-/* some all-round useful functions */
-	/**
-	 * adds a function to the onload event handler
-	 *
-	 * @param func			function to add
-	 */
-	
-	/**
-	 * document.getElementById() shortcut
-	 * 
-	 * @param id		element id
-	 */
-	function _(id)
-	{
-		return document.getElementById(id);
-	}
-	
-	/**
-	 * trim a string
-	 */
-	String.prototype.trim = function()
-	{
-		return this.replace(/^\s*|\s*$/g,"");
-	};
-	
-	/**
-	 * execute a function on every element with a specific tag in the container
-	 *
-	 * @param container		container element
-	 * @param tag			tag to match
-	 * @rapam func			function to execute
-	 */
-	forAll = function(container, tag, func)
-	{
-		var elArray = container.getElementsByTagName(tag);
-		var elArrayLength = elArray.length;
-		var i;
-		
-		for(i = 0; i < elArrayLength; i++)
-		{
-			elArray[i].exec = func;
-			elArray[i].exec();
-		}
-	};
-
-
-/**
- * some function to help with the dom
- */
-var dom =
-{
-	/**
-	 * checks if an element is the other element's parent
-	 *
-	 * @param el		reference element
-	 * @param parent	element maybe a parent of el
-	 */
-	hasParent : function(el, parent)
-	{
-		var parentEl = el.parentNode;
-		while(parentEl !==document.body)
-		{
-			if(parentEl === parent)
-			{
-				return true;
-			}
-			else
-			{
-				parentEl = parentEl.parentNode;
-			}
-		}
-		return false;
-	},
-	
-	tabIndexes : [null]
-};
-
 /* controls (spremeni, brisi, ...) */
-var controls =
-{
-	prepare : function()
+$(document).ready(function(){
+
+	var controls =
 	{
-		var divClass;
-		var textareaDiv = null;
-		
-		var editField = _('content_field');
-		if(editField)
+		prepare : function()
 		{
-			var controlsDiv = edit.toolbar();
-			edit.field = editField;
-			editField.parentNode.parentNode.insertBefore(controlsDiv, editField.parentNode);
+			var divClass;
+			var textareaDiv = null;
 			
-			
-			jQuery('#content_field:not(.processed)').each(function(){
-				var form_height = $.cookie('form_height');
-				if (form_height){
-					$(this).height(parseInt(form_height, 10));
-				}
-			});
-			jQuery('#content_field:not(.processed)').TextAreaResizer();
-		}
-	},
-	
-	to : null,
-	curEl : null,
-	menuShown : false,
-	
-	menu :
-	{
-		mouseover : function()
-		{
-			if (!controls.curEl)
+			var editField = $('#content_field').get(0);
+			if(editField)
 			{
-				controls.curEl = this.parentNode.parentNode;
-				controls.to = window.setTimeout("controls.menu.show()", 100);
-			}
-			else if(controls.menuShown && (controls.curEl === this || controls.curEl === this.parentNode.parentNode))
-			{
-				window.clearTimeout(controls.to);
-			}
-			else if(controls.curEl)
-			{
-				window.clearTimeout(controls.to);
-				controls.menu.hide();
-				controls.curEl = this.parentNode.parentNode;
-				controls.menu.show();
-			}
-		},
-		
-		mouseout : function()
-		{
-			if (controls.curEl && !controls.menuShown)
-			{
-				window.clearTimeout(controls.to);
-				controls.curEl = null;
-			}
-			else if(controls.menuShown)
-			{
-				window.clearTimeout(controls.to);
-				controls.to = window.setTimeout("controls.menu.hide()", 100);
-			}
-		},
-		
-		show : function()
-		{
-			if(controls.curEl)
-			{
-				controls.curEl.onmouseout = controls.menu.mouseout;
-				controls.curEl.onmouseover = controls.menu.mouseover;
-				controls.curEl.className = 'opened';
-				controls.menuShown = true;
-			}
-		},
-		
-		hide : function()
-		{
-			if(controls.curEl)
-			{
-				controls.curEl.onmouseout = null;
-				controls.curEl.onmouseover = null;
+				var controlsDiv = edit.toolbar();
+				edit.field = editField;
+				editField.parentNode.parentNode.insertBefore(controlsDiv, editField.parentNode);
 				
-				controls.curEl.className = 'closed';
-				controls.curEl = null;
-				controls.menuShown = false;
+				
+				$('#content_field:not(.processed)').each(function(){
+					var form_height = $.cookie('form_height');
+					if (form_height){
+						$(this).height(parseInt(form_height, 10));
+					}
+				});
+				$('#content_field:not(.processed)').TextAreaResizer();
 			}
-			window.clearTimeout(controls.to);
+		},
+		
+		to : null,
+		curEl : null,
+		menuShown : false,
+		
+		menu :
+		{
+			mouseover : function()
+			{
+				if (!controls.curEl)
+				{
+					controls.curEl = this.parentNode.parentNode;
+					controls.to = window.setTimeout("controls.menu.show()", 100);
+				}
+				else if(controls.menuShown && (controls.curEl === this || controls.curEl === this.parentNode.parentNode))
+				{
+					window.clearTimeout(controls.to);
+				}
+				else if(controls.curEl)
+				{
+					window.clearTimeout(controls.to);
+					controls.menu.hide();
+					controls.curEl = this.parentNode.parentNode;
+					controls.menu.show();
+				}
+			},
+			
+			mouseout : function()
+			{
+				if (controls.curEl && !controls.menuShown)
+				{
+					window.clearTimeout(controls.to);
+					controls.curEl = null;
+				}
+				else if(controls.menuShown)
+				{
+					window.clearTimeout(controls.to);
+					controls.to = window.setTimeout("controls.menu.hide()", 100);
+				}
+			},
+			
+			show : function()
+			{
+				if(controls.curEl)
+				{
+					controls.curEl.onmouseout = controls.menu.mouseout;
+					controls.curEl.onmouseover = controls.menu.mouseover;
+					controls.curEl.className = 'opened';
+					controls.menuShown = true;
+				}
+			},
+			
+			hide : function()
+			{
+				if(controls.curEl)
+				{
+					controls.curEl.onmouseout = null;
+					controls.curEl.onmouseover = null;
+					
+					controls.curEl.className = 'closed';
+					controls.curEl = null;
+					controls.menuShown = false;
+				}
+				window.clearTimeout(controls.to);
+			}
 		}
-	}
+	};
+
+if ($('#content_field').length > 0) {
+	controls.prepare();
 };
-onLoad(controls.prepare);
+
+});
